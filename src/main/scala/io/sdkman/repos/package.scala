4968038dfec30e0ac8582e6968fe760ab7504544
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-04-04 10:53:16 +0100

    Add repo package object with: * mongo execution context * implicits for doc <=> case class conversion.

---
+++ src/main/scala/io/sdkman/repos/package.scala
@@ -0,0 +1,41 @@
+package io.sdkman
+
+import java.util.concurrent.Executors
+
+import org.mongodb.scala.bson._
+
+import scala.concurrent.ExecutionContext
+import scala.language.implicitConversions
+
+package object repos {
+
+  implicit val mongoExecutionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(10))
+
+  val MissingField = "MISSING_FIELD"
+
+  implicit def documentToCandidate(doc: Document): Candidate =
+    Candidate(
+      field("candidate", doc),
+      field("name", doc),
+      field("description", doc),
+      field("default", doc),
+      field("websiteUrl", doc),
+      field("distribution", doc))
+
+  implicit def documentToVersion(doc: Document): Version =
+    Version(
+      field("candidate", doc),
+      field("version", doc),
+      field("platform", doc),
+      field("url", doc))
+
+  implicit def documentToApplication(doc: Document): Application =
+    Application(
+      field("alive", doc),
+      field("stableCliVersion", doc),
+      field("betaCliVersion", doc))
+
+  private def field(n: String, d: Document): String =
+    d.get[BsonString](n).map(_.asString.getValue).getOrElse(MissingField)
+}
+
